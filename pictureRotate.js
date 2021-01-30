f2829675e4859f430c0c55b1a541248e9ed4a3b5
Guest <guest-naxwh4@h26817.sgb.skir.kr>
2021-01-30 20:17:45 +0900

    storage

--- pictureRotate.js
+++ pictureRotate.js
@@ -2,12 +2,31 @@
 
 export class PictureRotate{
     constructor(){        
-        let pictures = [];
-        fetch('./image')
-            .then((data) => {
-               
-                console.log(data);
-            });
+        this.imageSection = document.querySelectorAll('.imageSection');
+        this.centerPos = {
+            centerX : document.body.clientWidth/2,
+            centerY : document.body.clientHeight/2,
+        }
+        this.radius = 300;
+
+        this.imageNum = 0;
+        this.scrollValue = 0;
+        this.isScroll = false;
+
+        this.init();
+        window.addEventListener('wheel',this.wheelEvent.bind(this));
     }
     
+    init(){
+        let deg;
+        for(let i = 0 ;  i < this.imageSection.length; i++){
+            deg = (45 * i/180)*Math.PI;
+            this.imageSection[i].style.left =`${this.centerPos.centerX + Math.cos(deg) * this.radius}px`;
+            this.imageSection[i].style.top =`${this.centerPos.centerY + Math.sin(deg) * this.radius}px`;
+        }
+    }
+
+    wheelEvent(data){
+        console.log(data.wheelDelta);
+    }
 }
