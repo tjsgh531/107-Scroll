ded1b35e26804c3eb6bc34e74723593721f6c325
Guest <guest-naxwh4@h26817.sgb.skir.kr>
2021-01-30 23:53:03 +0900

    need to repair wheelSpeed

--- pictureRotate.js
+++ pictureRotate.js
@@ -1,17 +1,23 @@
-
+/* 휠 스피드 부분을 보강 해야합니다. */
 
 export class PictureRotate{
     constructor(){        
         this.imageSection = document.querySelectorAll('.imageSection');
         this.centerPos = {
-            centerX : document.body.clientWidth/2,
-            centerY : document.body.clientHeight/2,
+            centerX : -300,
+            centerY : document.body.clientHeight/2 - 300,
         }
-        this.radius = 300;
+        this.radius = document.body.clientWidth /2;
 
         this.imageNum = 0;
         this.preImageNum = 0;
         this.wheelValue = 0;
+
+        this.isChecking = false;
+        this.wheelSpeed = 0;
+        this.transitionSpeed = 1 ;
+
+        this.wheelEventEnd = true;
         
         this.init();
         window.addEventListener('wheel',this.wheelEvent.bind(this));
@@ -24,20 +30,29 @@ export class PictureRotate{
             this.imageSection[i].style.left =`${this.centerPos.centerX + Math.cos(deg) * this.radius}px`;
             this.imageSection[i].style.top =`${this.centerPos.centerY + Math.sin(deg) * this.radius}px`;
         }
+
     }
 
     wheelEvent(data){
+
         this.wheelValue  -= data.wheelDelta;
         if(this.wheelValue < 0){
             this.wheelValue = 0;
         }
-        console.log(this.wheelValue);
-    
+
+        this.wheelSpeed -= data.wheelDelta;
+        
+        if(!this.isChecking){
+            this.initWheelSpeed();
+        }
+        
+
         this.imageNum = Math.floor(this.wheelValue / 840);
-        console.log(`imageNum = ${this.imageNum}`);
+
         if(this.preImageNum < this.imageNum){
             this.switchRegular();
         }
+        
         else if(this.preImageNum > this.imageNum){
             this.switchReverse();
         }
@@ -64,4 +79,22 @@ export class PictureRotate{
         this.preImageNum = this.imageNum;
     }
 
+    /*휠 속도가 빠르면 transition스피드도 빨라지게하는 메소드*/
+    initWheelSpeed(){
+        this.isChecking = ture;
+        setTimeout( () => {
+           if(this.wheelSpeed != 0){
+               const decentWheelSpeed = setInterval(()=>{
+                   if(this.wheelSpeed > 0){
+                        this.wheelSpeed -= 120;
+                   }
+                   else{
+                       clearInterval(decentWheelSpeed);
+                   }
+               },300);
+           }
+            this.isChecking = false;
+        },1000);
+        
+    }
 }
