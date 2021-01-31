814a7ae24e9a5dad6c181ace995a97915d081337
Guest <guest-zkp1ar@h26632.sgb.skir.kr>
2021-01-31 15:00:13 +0900

    add Change Image & InitImage

--- pictureChange.js
+++ pictureChange.js
@@ -3,10 +3,25 @@
 export class PictureChange{
     constructor(imgNum){
         this.imageSection = document.querySelectorAll('.imageSection');
+        
         this.preImageNum = imgNum;
         this.imageNum = imgNum;
-        this.centerChildNum = 0;
+
+        this.images = [];
+        this.initImageSet();
     }
+    initImageSet(){
+        for(let i = 0 ; i < this.imageSection.length; i++){
+            const item = new Image();
+            this.images.push(item);
+            this.imageSection[i].appendChild(item);
+        }
+
+        for(let i = 0 ; i< 6; i++){
+            this.images[i].src =`./image/${i}.jpg`;
+        }
+    }
+
     getImageNum(imgNum){
         this.imageNum = imgNum;
 
@@ -15,11 +30,27 @@ export class PictureChange{
             this.preImageNum = this.imageNum;
         }
     }
+
     changePicture(){
-        this.centerChildNum = this.imageNum % 8;
+        const centerChildNum = this.imageNum % 8;
+        const nextChildNum = (this.imageNum + 1) % 8;
+        let preChildNum;
+
+        if(this.imageNum -1 >= 0){
+            preChildNum = (this.imageNum -1 ) % 8;
+        }
+        else{
+            preChildNum = 7;
+        }
+
+        this.images[centerChildNum].src = `./image/${this.imageNum}.jpg`;
+        this.images[nextChildNum].src = `./image/${this.imageNum+1}.jpg`;
         
-        for(let i =0 ; i < 5 ; i++){
-            this.imageSection[this.centerChildNum].style.backgroundImage =`url('./image/${this.imageNum}.jpg)`;
+        if(this.imageNum -1 >= 0){
+            this.images[preChildNum].src = `./image/${this.imageNum -1}.jpg`;
+        }
+        else{
+            this.images[preChildNum].src = '';
         }
     }
 }
