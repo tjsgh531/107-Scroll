a6bfafe4e21b5af7a99429546cc8417724140dd7
Guest <guest-cj5khp@h26817.sgb.skir.kr>
2021-02-06 22:27:40 +0900

    complete AutoScroll

--- autoScroll.js
+++ autoScroll.js
@@ -1,27 +1,63 @@
+import{
+    dataBase
+}from './main.js';
+
 export class AutoScroll{
     constructor(){
         this.checkBox = document.getElementById('checkBox');
-        console.log(typeof(this.checkBox));
         this.sentence = document.querySelector('#autoScrollSwitch p');
-        this.checkBox.addEventListener('click',()=>{
-            this.switchStatus = this.checkBox.checked;
-            console.log(this.switchStatus);
-            if(this.switchStatus){
-                this.sentence.textContent = '자동스크롤 ON';
-                this.autoScroll();
-            }
-            else{
-                this.sentence.textContent ='자동스크롤 OFF';
-                this.stopAutoScroll();
-            }
-        })
+        this.canAutoScroll = false;
+        this.autoScrollTime = 4000;
+
+        this.checkBox.addEventListener('click',this.onClick.bind(this));
+    }
+
+    onClick(){
+        this.switchStatus = this.checkBox.checked;
+ 
+        if(this.switchStatus){
+            this.sentence.textContent = '자동스크롤 ON';
+            this.canAutoScroll = true;
+            this.isAutoScroll = true;
+            this.autoScroll();
+        }
+        else{
+            this.sentence.textContent ='자동스크롤 OFF';
+            this.isAutoScroll = false;
+            this.stopAutoScroll();
+        }
     }
 
     autoScroll(){
         console.log('autoScroll실행중');
+
+        if(this.canAutoScroll && this.isAutoScroll){
+            this.canAutoScroll = false;
+            let imageSectiontemp = [...dataBase.imageSection];
+            let temp;
+
+            dataBase.imageNum++;
+            if(dataBase.imageNum > dataBase.maxImageNum){
+                dataBase.imageNum--;
+            }
+            else{
+                temp = imageSectiontemp[0];
+                imageSectiontemp.shift();
+                imageSectiontemp.push(temp);
+                dataBase.imageSection = [...imageSectiontemp];
+            }
+
+            dataBase.changeSection();
+
+            setTimeout(() => {
+                this.canAutoScroll = true;
+                this.autoScroll();
+            },this.autoScrollTime);
+        }
     }
 
     stopAutoScroll(){
         console.log('stopAutoScroll실행중');
+        this.canAutoScroll = false;
     }
 }
